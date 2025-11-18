f1e9c06fa722c41494448609a7a01e2af6e52bd9
Brandon Beach <brandon.beach23@gmail.com>
2025-11-17 20:19:10 -0500

    Initial commit with Angular frontend and Spring Boot backend

---
+++ backend/plaid-finance-tracker-backend/src/main/java/io/github/brandonbeach23/plaid_finance_tracker_backend/PlaidFinanceTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.brandonbeach23.plaid_finance_tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaidFinanceTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaidFinanceTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/plaid-finance-tracker-backend/src/test/java/io/github/brandonbeach23/plaid_finance_tracker_backend/PlaidFinanceTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brandonbeach23.plaid_finance_tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaidFinanceTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
