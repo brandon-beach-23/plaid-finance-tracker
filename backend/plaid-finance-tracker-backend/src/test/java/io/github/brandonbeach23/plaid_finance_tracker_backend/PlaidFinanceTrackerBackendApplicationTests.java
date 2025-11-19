1b930a92f60a4aec925c4e40a9aa7d0a7f9df178
Brandon Beach <brandon.beach23@gmail.com>
2025-11-19 10:54:16 -0500

    Added environment variables and configured to the project and added a ci profile for tests and my ci pipeline.

--- backend/plaid-finance-tracker-backend/src/test/java/io/github/brandonbeach23/plaid_finance_tracker_backend/PlaidFinanceTrackerBackendApplicationTests.java
+++ backend/plaid-finance-tracker-backend/src/test/java/io/github/brandonbeach23/plaid_finance_tracker_backend/PlaidFinanceTrackerBackendApplicationTests.java
@@ -2,8 +2,10 @@ package io.github.brandonbeach23.plaid_finance_tracker_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("ci")
 class PlaidFinanceTrackerBackendApplicationTests {
 
 	@Test
